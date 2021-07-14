397cec893f534915ce14ac6187f84a7b0ee59006
sau114 <sau114@yandex.ru>
2021-07-14 18:48:37 +0300

    Add additional verifications.

--- main.py
+++ main.py
@@ -13,18 +13,18 @@ def print_welcome():
 
 
 def print_board():
-    print('Board:')
+    print()
     symbol_to_print = ''
     for i_row in range(BOARD_DIMENSION+1):
         for i_column in range(BOARD_DIMENSION+1):
-            if i_column == 0:    # в 0-м столбце - легенда
+            if i_column == 0:    # legend in 0 row
                 if i_row == 0:
                     symbol_to_print = ' '
                 else:
                     symbol_to_print = str(i_row-1)
-            elif i_row == 0:     # в 0-й строке - легенда
+            elif i_row == 0:     # legen in 0 column
                 symbol_to_print = str(i_column-1)
-            else:               # значащее поле доски
+            else:
                 if _board[i_row-1][i_column-1] == 1:
                     symbol_to_print = 'X'
                 elif _board[i_row-1][i_column-1] == 2:
@@ -33,6 +33,7 @@ def print_board():
                     symbol_to_print = '-'
             print(symbol_to_print, end=' ')
         print()
+    print()
     return 0
 
 
@@ -40,41 +41,54 @@ def ready_player(player_number):
     print('Player number ', player_number)
     while True:
         player_move = input('Player ' + str(player_number) + ' turn: ')
-        move_row, move_column = list(map(int, player_move.split()))
-        if (0 <= move_row < BOARD_DIMENSION) \
-            and (0 <= move_column < BOARD_DIMENSION):
-            if not _board[move_row][move_column]:
-                _board[move_row][move_column] = player_number
-                break
-            else:
-                print('Cell already occupied. Repeat your move.')
-        else:
+
+        player_move = player_move.split()
+        if len(player_move) != 2:
+            print('Must be 2 values. Repeat your move.')
+            continue
+
+        move_row, move_column = player_move
+        if not (move_row.isdigit() and move_column.isdigit()):
+            print('Must be numbers. Repeat your move.')
+            continue
+
+        move_row, move_column = list(map(int, player_move))
+        if move_row < 0 or move_row >= BOARD_DIMENSION \
+            or move_column < 0 or move_column >= BOARD_DIMENSION:
             print('Out of board. Repeat your move.')
+            continue
+
+        if _board[move_row][move_column]:
+            print('Cell already occupied. Repeat your move.')
+            continue
+
+        _board[move_row][move_column] = player_number
+        break
     return 0
 
 
 def player_won(player_number):
-    # проверим строки
+    # check rows
     for i_row in range(BOARD_DIMENSION):
         this_row = _board[i_row]
         if this_row.count(player_number) == BOARD_DIMENSION:
             print('Player filled row ' + str(i_row) + '.')
             return True
 
-    # проверим столбцы
+    # check columns
     for i_column in range(BOARD_DIMENSION):
         this_column = [_board[i_row][i_column] for i_row in range(BOARD_DIMENSION)]
         if this_column.count(player_number) == BOARD_DIMENSION:
             print('Player filled column ' + str(i_column) + '.')
             return True
 
-    # проверим главную диагональ
+    # check main diagonal
     this_diagonal = [_board[i][i] for i in range(BOARD_DIMENSION)]
     if this_diagonal.count(player_number) == BOARD_DIMENSION:
         print('Player filled main diagonal.')
         return True
 
-    # проверим побочную диагональ
+    # check side diagonal
     this_diagonal = [_board[BOARD_DIMENSION - i - 1][i] for i in range(BOARD_DIMENSION)]
     if this_diagonal.count(player_number) == BOARD_DIMENSION:
         print('Player filled side diagonal.')
@@ -84,7 +98,7 @@ def player_won(player_number):
 
 
 def move_possible():
-    # есть пустое поле для хода
+    # try to find free cell
     for i_row in range(BOARD_DIMENSION):
         for i_column in range(BOARD_DIMENSION):
             if not _board[i_row][i_column]:
